327ac23368f31e85fb387b9da3ac5a3db3c96725
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-09-26 19:28:48 -0700

    Updated handlebars, routes and public folder

--- models/Blogs.js
+++ models/Blogs.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Blogs extends Model {}
 
-Project.init(
+Blogs.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -40,8 +40,8 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'blogs',
   }
 );
 
-module.exports = Project;
+module.exports = Blogs;
