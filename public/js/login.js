eb31b34539e558c68132879222a756d36abdb0da
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-10-02 17:06:05 -0700

    Updated js code for handle bars

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,43 @@
+// Imports Handlebars
+const Handlebars = require('handlebars');
+
+// Function to handle the login form 
 const loginFormHandler = async (event) => {
     event.preventDefault();
-  
+
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  
+
     if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in');
+        }
     }
-  };
+};
 
-    // Grabs Handlebars
-    const templateform = document.getElementById("login-form").innerHTML
-    // Compile the Handlebars 
-    const template = Handlebars.compile(templateform);
-    const templateData = 
-    
-    {
-        user.name: "Enter your username",
-        user.password: "Enter your password",
-        user.button: "Login"
-    };
+// Grabs Handlebars template
+const templateform = document.getElementById("login-form").innerHTML;
+const template = Handlebars.compile(templateform);
 
-    const renderedHTML = template(templateData);
-    document.querySelector("login-form").innerHTML = renderedHTML;
-  
-  document.querySelector('.Login-section')
-    .addEventListener('submit', loginFormHandler);
+const templateData = {
+    user: {
+        name: "Enter your username",
+        password: "Enter your password",
+        button: "Login"
+    }
+};
+
+const renderedHTML = template(templateData);
+
+document.getElementById("login-form").innerHTML = renderedHTML;
+document.querySelector('.Login-section form').addEventListener('submit', loginFormHandler);
+
   
