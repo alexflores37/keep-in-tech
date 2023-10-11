42f23184edccabed7c101e14f057784caea814eb
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-10-10 18:12:53 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 let sequelize;
 
+//connecction for JAWSDB add on for local host connection for heroku
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
